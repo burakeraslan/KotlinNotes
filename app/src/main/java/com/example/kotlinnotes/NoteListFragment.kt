29f4b9612b9768311ea984c65290ff1f50face74
burak <buraakeraslan@outlook.com>
2024-04-30 22:19:03 +0300

    create - fragments - navigation - toolbar

---
+++ app/src/main/java/com/example/kotlinnotes/NoteListFragment.kt
@@ -0,0 +1,30 @@
+package com.example.kotlinnotes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class NoteListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.addButton).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_noteListFragment_to_noteDetailFragment)
+        }
+    }
+}
