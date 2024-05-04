e0d54c296f8ada47a0f25a8de16a23398046fc8c
burak <buraakeraslan@outlook.com>
2024-05-04 18:16:03 +0300

    db

--- app/src/main/java/com/example/kotlinnotes/NoteAddFragment.kt
+++ app/src/main/java/com/example/kotlinnotes/NoteAddFragment.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinnotes
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase.openOrCreateDatabase
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.navigation.Navigation
 
 class NoteAddFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,11 +40,30 @@ class NoteAddFragment : Fragment() {
             editTextNoteDescription.clearFocus()
         }
 
+        val db = requireActivity().applicationContext.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null)
+        db.execSQL("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY, header TEXT, description TEXT)")
+
+        fun handleSaveNote() {
+            val header = editTextNoteHeader.text.toString()
+            val description = editTextNoteDescription.text.toString()
+            db.execSQL("INSERT INTO notes (header, description) VALUES ('$header', '$description')")
+            println("Note saved")
+
+            val cursor = db.rawQuery("SELECT * FROM notes", null)
+            val idIndex = cursor.getColumnIndex("id")
+            val headerIndex = cursor.getColumnIndex("header")
+            val descriptionIndex = cursor.getColumnIndex("description")
+            while (cursor.moveToNext()) {
+                println("id: ${cursor.getInt(idIndex)}")
+                println("header: ${cursor.getString(headerIndex)}")
+                println("description: ${cursor.getString(descriptionIndex)}")
+            }
+        }
+
         val backButton = view.findViewById<View>(R.id.backButton)
         backButton.setOnClickListener {
-            println(
-                editTextNoteHeader.text.toString() + " " + editTextNoteDescription.text.toString()
-            )
+            handleSaveNote()
+            Navigation.findNavController(view).navigate(R.id.action_noteAddFragment_to_noteListFragment)
         }
 
     }
